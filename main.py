94f007f46c928cd91a6c5d0b9647edc6b6cc07b6
matsuda1380 <matsuda1380@gmail.com>
2021-01-07 16:36:55 +0900

    Won't Open Heroku due "git push heroku main" failure

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
-import requests
-from flask import request
+# import requests
+# from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return "Hello"
+
+@app.route('/background')
+def background():
     return render_template("index.html")
 
 
